f2d6116a01d6bdc5bf1b4cdb3be9b8a6cdf02ddf
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-09-29 22:44:29 -0700

    add DB_URL checks

--- api/src/config/env.ts
+++ api/src/config/env.ts
@@ -4,8 +4,18 @@ dotenv.config();
 
 const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
+const dbUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DB_URL
+    : process.env.DB_URL;
+
+if (!dbUrl) {
+  throw new Error("Database url is not defined");
+}
+
 // check if local env and validate credentials
 const isLocal = !process.env.CI && process.env.NODE_ENV !== "production";
+
 if (isLocal && !credentialsPath) {
   throw new Error("Path to GCP credentials must be defined in a local env");
 }
@@ -15,6 +25,8 @@ const systemPromptPath = process.env.SYS_PROMPT_PATH;
 if (!systemPromptPath) {
   throw new Error("System prompt is missing");
 }
+
+export const DB_URL = dbUrl;
 
 export const GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
 export const GCP_REGION = process.env.GCP_REGION;
