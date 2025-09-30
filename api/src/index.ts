5283ab095cad8adfc3917ce6e9efde42711bdaf2
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-09-29 22:44:44 -0700

    create db connection

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { CLIENT_ORIGIN, PORT } from "./config/env";
 import articleRouter from "./modules/articles/router";
+import { initDatabase } from "./db/client";
 
 const app = express();
 
@@ -18,6 +19,12 @@ app.use(express.json());
 // routers
 app.use("/api/v1/articles", articleRouter);
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}`);
-});
+const start = async () => {
+  await initDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+  });
+};
+
+start();
