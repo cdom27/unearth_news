82d479f31bd00ac8069496623825ea975ad4fa15
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-09-30 21:11:04 -0700

    create slugify util

---
+++ api/src/modules/articles/utils/slugify-title.ts
@@ -0,0 +1,26 @@
+// Mimic nanoId generation
+const nanoId = (length: number) => {
+  const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return id;
+};
+
+// Normalize title into a unique slug friendly string
+const slugify = (title: string) => {
+  const slugifiedTitle = title
+    .toLowerCase()
+    .normalize("NFKD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "")
+    .replace(/--+/g, "-");
+
+  const id = nanoId(6);
+
+  return slugifiedTitle.concat("-", id);
+};
+
+export default slugify;
