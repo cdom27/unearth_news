24a42b044edbbf7b8ec20ff3e573e1e0432d256e
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-10-14 16:23:55 -0700

    update py on main element

--- client/src/components/layouts/site-layout.tsx
+++ client/src/components/layouts/site-layout.tsx
@@ -43,7 +43,7 @@ const SiteLayout = ({ children }: SiteLayoutProps) => {
         </div>
       </header>
 
-      <main className="min-h-screen flex flex-col items-center justify-center">
+      <main className="min-h-screen py-25">
         <div
           onClick={() => setOpen(false)}
           className={`fixed inset-0 transition-opacity duration-500 ease-in-out bg-fg-dark ${
