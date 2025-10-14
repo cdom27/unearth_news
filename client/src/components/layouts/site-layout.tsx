4cd2cf35372a6e4138f2c04e1155d3640b84fb5b
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-10-14 00:51:07 -0700

    mobile nav

--- client/src/components/layouts/site-layout.tsx
+++ client/src/components/layouts/site-layout.tsx
@@ -1,18 +1,30 @@
-import type { ReactNode } from "react";
+import { useState, type ReactNode } from "react";
+import { Link } from "react-router";
+import Nav from "../navigation/nav";
 
 interface SiteLayoutProps {
   children: ReactNode;
 }
 
 const SiteLayout = ({ children }: SiteLayoutProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <>
-      <header>nav</header>
+    <div className={`p-5 grainy-bg ${open ? "h-screen overflow-hidden" : ""}`}>
+      <header className="flex justify-between items-center">
+        <Link to="/" className="text-xl font-medium">
+          Unearth
+        </Link>
+
+        <Nav onClick={() => setOpen(!open)} open={open} />
+      </header>
+
       <main className="min-h-screen flex flex-col items-center justify-center">
         {children}
       </main>
+
       <footer>footer</footer>
-    </>
+    </div>
   );
 };
 
---
+++ client/src/components/navigation/nav.tsx
@@ -0,0 +1,62 @@
+import { ListIcon, XIcon } from "@phosphor-icons/react";
+import { Link, NavLink } from "react-router";
+import { navLinks } from "../../utils/nav-links";
+
+interface NavProps {
+  onClick: (a: boolean) => void;
+  open: boolean;
+}
+
+const Nav = ({ onClick, open }: NavProps) => {
+  return (
+    <nav>
+      <button onClick={() => onClick(!open)}>
+        <ListIcon className="size-6 fill-fg-dark" />
+      </button>
+
+      <div
+        className={`fixed inset-0 z-10 p-5 transition-transform duration-500 ease-in-out bg-bg-dark-secondary text-fg-light
+    ${open ? "translate-x-0" : "-translate-x-full"}`}
+      >
+        <div className="flex justify-between pb-5 border-b-1 border-fg-dark-secondary">
+          <Link to="/" className="text-xl font-medium">
+            Unearth
+          </Link>
+
+          <button onClick={() => onClick(!open)}>
+            <XIcon className="size-6 fill-fg-light" />
+          </button>
+        </div>
+
+        {navLinks.map((category) => (
+          <section
+            key={category.name}
+            className="py-2 border-b-1 border-fg-dark-secondary"
+          >
+            <h2 className="text-xl text-fg-dark-secondary">{category.name}</h2>
+
+            <ul className="flex flex-col pt-3 gap-1">
+              {category.links.map((link) => (
+                <li key={link.href}>
+                  <NavLink
+                    to={link.href}
+                    className={({ isActive }) =>
+                      (isActive
+                        ? "text-brand-primary/90 underline decoration-dotted"
+                        : "hover:underline hover:decoration-dotted") +
+                      " text-2xl"
+                    }
+                  >
+                    {link.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </section>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
