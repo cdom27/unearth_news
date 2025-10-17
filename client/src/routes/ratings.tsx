8e9423a2f13d8c167efcc199ac14acd7830db20e
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-10-17 11:21:44 -0700

    add media-ratings to router

---
+++ client/src/routes/ratings.tsx
@@ -0,0 +1,13 @@
+import SiteLayout from "../components/layouts/site-layout";
+
+const RatingsPage = () => {
+  return (
+    <SiteLayout>
+      <h1 className="font-instrument text-5xl tracking-[.0125em]">
+        Hello world
+      </h1>
+    </SiteLayout>
+  );
+};
+
+export default RatingsPage;
