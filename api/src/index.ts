c19abd956c641ba8abe659ad5bd98e2ac92fb2d5
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-10-16 23:25:14 -0700

    implement endpoint to fetch all uniquely-referenced sources

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,12 @@ import express from "express";
 import cors from "cors";
 import { CLIENT_ORIGIN, PORT } from "./config/env";
 import articleRouter from "./modules/articles/router";
+import sourceRouter from "./modules/sources/router";
 import { initDatabase } from "./db/client";
 
 const app = express();
 
-// middlwares
+// middlewares
 app.use(
   cors({
     origin: CLIENT_ORIGIN,
@@ -18,6 +19,7 @@ app.use(express.json());
 
 // routers
 app.use("/api/v1/articles", articleRouter);
+app.use("/api/v1/sources", sourceRouter);
 
 const start = async () => {
   await initDatabase();
--- api/src/modules/sources/service.ts
+++ api/src/modules/sources/service.ts
@@ -1,10 +1,17 @@
 import { SourceRepository } from "./repository";
 import type { Result } from "../shared/types/service-result";
+import type { SourceFilterDTO } from "@shared/dtos/source";
+
+// Find all unique sources that have been references by an article
+export const findUsedSources = async (): Promise<Result<SourceFilterDTO[]>> => {
+  const sources = await SourceRepository.findUsed();
+  return { success: true, data: sources };
+};
 
 // Find sources by their bias and slug
 export const findFilteredSourceIds = async (
   slugs?: string[],
-  bias?: string[]
+  bias?: string[],
 ): Promise<Result<string[]>> => {
   let conditions: string[] = [];
   let params: any[] = [];
