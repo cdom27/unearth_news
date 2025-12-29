228dc9026af224029ebb7b6b0198da8b7e0d64dd
Cristian Dominguez <c.isaacdominguez@gmail.com>
2025-12-28 21:41:49 -0800

    add build optimizations

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    minify: "esbuild",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router"],
+          icons: ["@phosphor-icons/react"],
+        },
+      },
+    },
+  },
 });
