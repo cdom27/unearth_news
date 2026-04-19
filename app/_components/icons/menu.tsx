a9fb9716843eb7a247db48dfde85aa2fe922038a
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-04-18 23:10:02 -0700

    create menu icon

---
+++ app/_components/icons/menu.tsx
@@ -0,0 +1,16 @@
+import type { IconProps } from "@/app/_lib/types/icon-props";
+
+export default function MenuIcon({ className = "size-8" }: IconProps) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 32 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="31.3903" height="1.55556" fill="currentColor" />
+      <rect y="6.22217" width="31.3903" height="1.55556" fill="currentColor" />
+      <rect y="12.4443" width="31.3903" height="1.55556" fill="currentColor" />
+    </svg>
+  );
+}
