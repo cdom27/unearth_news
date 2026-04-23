d51833b4ef5ef4dfa438882afd5b1d72c3b2e47d
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-04-23 15:53:01 -0700

    implement gallery with local data (for now) using the article card components

---
+++ app/_components/ui/breaking-news/breaking-news-gallery.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { breakingNews } from "@/app/_lib/static/temp/breaking-news";
+import { NewsAPIArticle } from "@/app/_lib/types/news-api";
+import { useEffect, useState, useRef, useCallback } from "react";
+import CircleNotchIcon from "../../icons/circle-notch";
+import BreakingNewsCard from "../article-cards/breaking-news-card";
+import ArticleBadge from "../article-cards/article-badge";
+
+export default function BreakingNewsGallery() {
+  const [breakingNewsArticles, setBreakingNewsArticles] = useState<
+    NewsAPIArticle[]
+  >([]);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const pageSize = 9;
+
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  // temp static-data fetching with debounce within 200ms - 1000ms
+  useEffect(() => {
+    async function getArticles(pageNumber: number) {
+      const newArticles = breakingNews.slice(
+        pageNumber * pageSize,
+        (pageNumber + 1) * pageSize,
+      );
+
+      if (newArticles.length === 0) {
+        setHasMore(false);
+        return;
+      }
+
+      if (newArticles.length < pageSize) {
+        setHasMore(false);
+      }
+
+      setTimeout(
+        () => {
+          setBreakingNewsArticles((prev) => {
+            const existingUrls = new Set(prev.map((a) => a.url));
+            const filteredNewArticles = newArticles.filter(
+              (a) => !existingUrls.has(a.url),
+            );
+            return [...prev, ...filteredNewArticles];
+          });
+        },
+        Math.floor(Math.random() * (1000 - 200 + 1)) + 200,
+      );
+    }
+
+    if (hasMore) {
+      getArticles(page);
+    }
+  }, [page, hasMore]);
+
+  const lastArticleRef = useCallback(
+    (node: HTMLDivElement | null) => {
+      if (!hasMore) return;
+
+      if (observer.current) {
+        observer.current.disconnect();
+      }
+
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+
+      if (node) {
+        observer.current.observe(node);
+      }
+    },
+    [hasMore],
+  );
+
+  return (
+    <article className="flex flex-col gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
+        {breakingNewsArticles.map((article) => (
+          <button
+            key={article.url}
+            onClick={() =>
+              console.log("analyzing article URL: ", article.url || "")
+            }
+            className="group hover:cursor-pointer rounded-sm flex"
+            aria-label={`Analyze: ${article.title}`}
+          >
+            <BreakingNewsCard
+              title={article.title}
+              excerpt={article.description}
+              sourceName={article.source.name}
+              publishedTime={article.publishedAt}
+              thumbnailURL={article.urlToImage}
+              articleURL={article.url}
+              badge={
+                article.publishedAt && (
+                  <ArticleBadge
+                    variant="time"
+                    timeStamp={article.publishedAt}
+                  />
+                )
+              }
+            />
+          </button>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-1 col-span-3 gap-1.5">
+        {hasMore && <CircleNotchIcon className="size-6 animate-spin mx-auto" />}
+        <p ref={lastArticleRef} className="text-center">
+          Showing {breakingNewsArticles.length} of {breakingNews.length}{" "}
+          articles
+        </p>
+      </div>
+    </article>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import BreakingNewsGallery from "./_components/ui/breaking-news/breaking-news-gallery";
+
 export default function Home() {
   return (
     <>
-      <section className="m-4 sm:my-6 sm:mx-12 md:my-10 xl:my-16 2xl:my-22 pb-8 sm:pb-12 md:pb-16 xl:pb-22 2xl:pb-28 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col border-b border-clay-200">
+      <section className="m-4 sm:my-6 sm:mx-12 md:my-10 xl:my-16 2xl:my-22 pb-8 sm:pb-12 md:pb-16 xl:pb-22 2xl:pb-28 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col gap-12 border-b border-clay-200">
         <div className="flex flex-col gap-6 items-center">
           <h1 className="text-6xl lg:text-7xl font-serif text-center">
             Clarity in Every Narrative
@@ -16,7 +18,18 @@ export default function Home() {
         <div>{/*ANALYSIS FORM*/}</div>
       </section>
 
-      <section>{/*NEWS API QUERYING HERE*/}</section>
+      <article className="m-4 sm:my-6 sm:mx-12 md:my-10 xl:my-16 2xl:my-22 pb-8 pt-4 sm:pb-12 sm:py-6 md:pb-16 xl:pb-22 2xl:pb-28 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col gap-12 border-b border-clay-200">
+        <div className="flex flex-col gap-6">
+          <h2 className="text-4xl font-serif text-center sm:text-left">
+            Breaking News
+          </h2>
+          <p className="sr-only">
+            Latest breaking news stories from trusted sources.
+          </p>
+        </div>
+
+        <BreakingNewsGallery />
+      </article>
     </>
   );
 }
