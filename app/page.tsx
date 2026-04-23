b0bc11c163e80db85979f5882b3cfda55845f4e4
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-04-23 16:04:21 -0700

    update spacing on final section of the page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <div>{/*ANALYSIS FORM*/}</div>
       </section>
 
-      <article className="m-4 sm:my-6 sm:mx-12 md:my-10 xl:my-16 2xl:my-22 pb-8 pt-4 sm:pb-12 sm:py-6 md:pb-16 xl:pb-22 2xl:pb-28 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col gap-12 border-b border-clay-200">
+      <article className="mx-4 mt-4 sm:mt-6 sm:mx-12 md:mt-10 xl:mt-16 2xl:mt-22 pt-4 sm:pt-6 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col gap-12">
         <div className="flex flex-col gap-6">
           <h2 className="text-4xl font-serif text-center sm:text-left">
             Breaking News
