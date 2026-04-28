47b1a4e83bd03361cd4d6d32691106bcc3cf77f5
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-04-27 17:07:31 -0700

    add blocklist check in the URL normalization

--- app/_lib/normalize-url.ts
+++ app/_lib/normalize-url.ts
@@ -1,5 +1,12 @@
-export function normalizeURL(input: string): string {
+import { urlBlocklist } from "./static/url-blocklist";
+
+export function normalizeURL(input: string): string | null {
   const url = new URL(input);
+
+  if (urlBlocklist.includes(url.hostname)) {
+    return null;
+  }
+
   const hostname = url.hostname.toLowerCase().replace(/^www\./, "");
   const pathname = url.pathname.replace(/\/$/, "");
   return `${url.protocol}//${hostname}${pathname}`;
