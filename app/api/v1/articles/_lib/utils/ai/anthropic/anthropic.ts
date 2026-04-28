a37ce50a400986534667e8c41e4a8191de90e51f
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-04-27 17:12:15 -0700

    create flexible anthropic client to support predefined models + use cases

---
+++ app/api/v1/articles/_lib/utils/ai/anthropic/anthropic.ts
@@ -0,0 +1,32 @@
+import Anthropic from "@anthropic-ai/sdk";
+import fs from "fs/promises";
+import path from "path";
+
+export async function anthropic(
+  model: "claude-haiku-4-5" | "claude-sonnet-4-6",
+  mode: "summarize" | "analyze" | "extract",
+  userMsg: string,
+) {
+  const anth = new Anthropic();
+
+  const systemPrompt = await fs.readFile(
+    path.join(process.cwd(), "local", `${mode}.md`),
+    "utf-8",
+  );
+
+  const msg = await anth.messages.create({
+    model: model,
+    max_tokens: 1000,
+    system: systemPrompt,
+    messages: [
+      {
+        role: "user",
+        content: userMsg,
+      },
+    ],
+  });
+
+  const firstBlock = msg.content[0];
+
+  return firstBlock.type === "text" ? firstBlock.text : "";
+}
